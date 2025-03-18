1eaba1ca8aa69f316359f4497b146bb518fc74aa
kate_utkate <kate_utkate@vk.com>
2025-03-18 17:02:31 +0300

    initial

---
+++ app/web/app.py
@@ -0,0 +1,38 @@
+from typing import Optional
+from aiohttp_apispec import setup_aiohttp_apispec
+
+from aiohttp.web import Application as AiohttpApplication, View as AiohttpView, Request as AiohttpRequest
+from aiohttp.web import run_app as aiohttp_run_app
+
+from app.store import setup_accessors
+from app.store.crm.accessor import CrmAccessor
+from app.web.middlewares import setup_middlewares
+from app.web.routes import setup_routes
+
+
+class Application(AiohttpApplication):
+    database: dict = {}
+    crm_accessor: Optional[CrmAccessor] = None
+
+
+class Request(AiohttpRequest):
+    @property
+    def app(self) -> Application:
+        return super().app()
+
+
+class View(AiohttpView):
+    @property
+    def request(self) -> Request:
+        return super().request
+
+
+app = Application()
+
+
+def run_app():
+    setup_routes(app)
+    setup_aiohttp_apispec(app, title="CRM Application", url="/docs/json", swagger_path="/docs")
+    setup_middlewares(app)
+    setup_accessors(app)
+    aiohttp_run_app(app)
